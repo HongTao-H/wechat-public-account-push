8e360cca8756d4df82a8892cde9a4e08f6738b13
HongTao-H <74543103+HongTao-H@users.noreply.github.com>
2023-04-24 15:54:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o2-S45h45htSjvH1bSb2aWQ7WnO8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'JmVc1K6F8wnBX-pKNNBjm5O6bVFLYmBq1rZ5PL4FDis',
+      useTemplateId: 'xonsDx4ex_u6Ja6vHzw-_Tc3f8dmXoFSyDqSFbleWKM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
